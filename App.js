359de8a4f70ebfbcb134f387c75b851d0cb9e410
vladosxxx <vladosxxx@gmail.com>
2020-10-02 15:18:43 +0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect } from 'react';
+import { ActivityIndicator, StyleSheet, Text, View, Image, FlatList } from 'react-native';
+import Unsplash, { toJson } from 'unsplash-js'
 
 export default function App() {
-  return (
+  const [isLoading, setLoading] = useState(true);
+  const [imgs, setImg] = useState([]);
+
+  useEffect (()=>{
+    const unsplash = new Unsplash({
+    accessKey: "7SI75r0Sdp9V-rT7tOLGF4AdEs7j4764GpQn_4VpMk4"
+    });
+    unsplash.photos.listPhotos(1, 30)
+      .then(toJson)
+      .then(data => {
+        setImg(data)})
+      .catch(error => console.error(error))
+      .finally(() => setLoading(false))
+    }, []
+  )
+return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={imgs}
+          keyExtractor={( item ) => item.id}
+          renderItem={({item})  => 
+            <View style={styles.item}>
+              <Image
+              style={{width: 100, height: 100}}
+              source={{ uri: item.urls.small}} 
+                />
+              <Text style={styles.text}>{item.user.name}</Text>
+            </View>
+          }
+        />
+      )}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +43,24 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 20,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flexDirection: 'column',
+    backgroundColor: 'black',
+    alignItems: 'flex-start',
     justifyContent: 'center',
   },
+  text: {
+    color: '#fff',
+    fontSize: 16,
+    marginLeft: 20
+  },
+  item: {
+    borderColor: 'white',
+    borderWidth: 1,
+    margin: 10,
+    padding: 6,
+    flexDirection: 'row',
+    alignItems: 'center'
+  }
 });
